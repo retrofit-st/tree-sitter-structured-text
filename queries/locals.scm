11607db2ecbcd9f869116cc49865472cab1762fe
Michael Tal <mi-tal.dev@pm.me>
2024-07-02 19:01:13 +0200

    add function name as local definition

--- queries/locals.scm
+++ queries/locals.scm
@@ -4,5 +4,6 @@
 (function_declaration) @local.scope
 (function_block_declaration) @local.scope
 (var_declaration name: (identifier) @local.definition)
+(function_declaration name: (identifier) @local.definition)
 
 (identifier) @local.reference
